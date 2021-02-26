124ab6397be2e38e1ec711b3119202a6fc1b2e4d
Arthur <thejamesssmith@gmail.com>
2021-02-26 23:09:37 +0200

    got peer chat working

--- Chat.js
+++ Chat.js
@@ -10,6 +10,7 @@ class Chat
          * @type {Socket[]}
          */
         this.users = []
+        this.peerIds = []
     }
     /**
      * 
@@ -20,12 +21,33 @@ class Chat
         console.log('Added user');
         this.sendMessage("Server", "Пользователь подключился")
         this.users.push(user)
+        user.emit('sucess')
         user.on('message', (name, text) => this.sendMessage(name, text))
         user.on('disconnect', () =>
         {
             this.sendMessage("Server", "Пользователь отключился")
             console.log('A user left');
             this.users = this.users.filter(socket => socket !== user)
+        })
+        this.users.forEach(oldUser =>
+        {
+            const peerId = oldUser.peerId
+            if (peerId !== undefined)
+            {
+                console.log('Emitted ' + peerId);
+                user.emit('peerId', peerId)
+            }
+        })
+        user.on('peerId', peerId =>
+        {
+            console.log('Got a peer id ' + peerId);
+            user.peerId = peerId
+            console.log(user.peerId);
+            const index = this.users.findIndex(Olduser => Olduser === user)
+            for (let i = index + 1; i < this.users.length; i++)
+            {
+                this.users[i].emit('peerId', peerId)
+            }
         })
     }
     sendMessage(name, text)
